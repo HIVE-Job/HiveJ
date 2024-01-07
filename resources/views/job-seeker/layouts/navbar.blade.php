34497def51f417d0bd80c1d9d3e75779c385b3bb
Githubprojectsource123 <githubproject1234@gmail.com>
2024-01-07 20:33:22 +0530

    Updating files step 1

--- resources/views/job-seeker/layouts/navbar.blade.php
+++
@@ -1,9 +0,0 @@
-<nav class="mx-auto site-navigation">
-  <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
-    <li><a href="{{route('u.homepage')}}" class="nav-link {{ request()->is('user/beranda') ? 'active' : ''}}">Home</a></li>
-    <li><a href="{{route('u.vacancies')}}" class="nav-link {{ request()->is('user/vacancy') || request()->is('user/vacancy/*') || request()->is('user/vacancy-freelancer') || request()->is('user/vacancy-fulltime') ? 'active' : ''}}">All Jobs</a></li>
-    <li><a href="{{route('u.applications')}}" class="nav-link {{ request()->is('user/lamaran') || request()->is('user/lamaran/*') ? 'active' : ''}}">My Applications</a></li>
-    <li><a href="{{route('u.notifications')}}" class="nav-link {{ request()->is('user/notifikasi') ? 'active' : ''}}">Notifications @if($unread_notif > 0)({{ $unread_notif }})@endif</a></li>
-  </ul>
-</nav>
-
--- resources/views/recruiter/layouts/navbar.blade.php
+++
@@ -1,8 +0,0 @@
-<nav class="mx-auto site-navigation">
-    <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
-        <li><a href="{{route('r.homepage')}}" class="nav-link  {{ request()->is('recruiter/beranda') ? 'active' : ''}}">Home</a></li>
-        <li><a href="{{route('r.show_vacancies')}}" class="nav-link  {{ request()->is('recruiter/manage-job-vacancies') ? 'active' : ''}}">Manage Job Vacancies</a></li>
-        <li><a href="{{route('r.show_applications')}}" class="nav-link  {{ request()->is('recruiter/manage-job-applications') ? 'active' : ''}}">Manage Job Applications</a></li>
-        <li><a href="{{route('r.notifications')}}" class="nav-link {{ request()->is('recruiter/notifikasi') ? 'active' : ''}}">Notifications @if($unread_notif > 0)({{ $unread_notif }})@endif</a></li>
-    </ul>
-</nav>
